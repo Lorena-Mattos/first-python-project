4bb1da5e98629503a2ed18f439f7e49a0b29d0a6
Dpbm <dpbm136@gmail.com>
2022-12-29 20:26:12 -0300

    fixed formatted string

--- calc-imc.py
+++ calc-imc.py
@@ -3,11 +3,11 @@
 nome   = input('Qual é seu nome? ').capitalize()
 idade  = input('Qual é sua idade? ')
 altura = float(input('Qual é a sua altura? '))
-peso   = int(input('Quanto você pesa? '))
+peso   = float(input('Quanto você pesa? '))
 
 imc = peso / altura ** 2
 
-result = f'{nome} tem {altura:.2f} de altura, sua idade é {idade} anos e pesa {peso} Kg. Seu IMC é: {imc}'
+result = f'{nome} tem {altura:.2f} de altura, sua idade é {idade} anos e pesa {peso:.2f} Kg. Seu IMC é: {imc:.2f}'
 print(result)
 
 magreza   = 18.5
