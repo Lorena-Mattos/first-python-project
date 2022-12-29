d4aadc5c9aaddd17832cd073c79abba0ad39c575
Dpbm <dpbm136@gmail.com>
2022-12-29 20:20:28 -0300

    fixed variable names

--- calc-imc.py
+++ calc-imc.py
@@ -1,19 +1,14 @@
 # Exercício para calcular IMC
 
-nome = input('Qual é seu nome? ').capitalize()
+nome   = input('Qual é seu nome? ').capitalize()
+idade  = input('Qual é sua idade? ')
+altura = float(input('Qual é a sua altura? '))
+peso   = int(input('Quanto você pesa? '))
 
-idade = input('Qual é sua idade? ')
-
-altura = input('Qual é a sua altura? ')
-float_altura = float(altura)
-
-peso = input('Quanto você pesa? ')
-int_peso = int(peso)
-
-imc = int_peso / float_altura ** 2
+imc = peso / altura ** 2
 
 
-result = f'{nome} tem {float_altura:.2f} de altura, sua idade é {idade} anos e pesa {int_peso} Kg. Seu IMC é: {imc}'
+result = f'{nome} tem {altura:.2f} de altura, sua idade é {idade} anos e pesa {peso} Kg. Seu IMC é: {imc}'
 print(result)
 
 magreza = 18.5
