12ad7d4dc5b233113a69dfa5aeb41dd59f0addcb
Dpbm <dpbm136@gmail.com>
2022-12-29 20:23:27 -0300

    fixed prints

--- calc-imc.py
+++ calc-imc.py
@@ -7,26 +7,25 @@ peso   = int(input('Quanto você pesa? '))
 
 imc = peso / altura ** 2
 
-
 result = f'{nome} tem {altura:.2f} de altura, sua idade é {idade} anos e pesa {peso} Kg. Seu IMC é: {imc}'
 print(result)
 
-magreza = 18.5
-normal = 19
+magreza   = 18.5
+normal    = 19
 sobrepeso = 29
 obesidade = 30 
 
 if imc < magreza:
-    print(nome, 'cuidado, seu IMC indica magreza')
+    print(f"{nome} cuidado, seu IMC indica magreza")
     
 elif imc >= normal:
-    print(nome, 'seu IMC está normal')
+    print(f"{nome} seu IMC está normal")
 
 elif imc >= sobrepeso:
-    print(nome, 'seu IMC indica sobrepeso')
+    print(f"{nome} seu IMC indica sobrepeso")
 
 elif imc > obesidade:
-    print(nome, 'cuidado, seu IMC indica obesidade')
+    print(f"{nome} cuidado, seu IMC indica obesidade")
 
 else:
     print('Não há indicações de IMC')
